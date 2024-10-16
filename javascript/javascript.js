38378501c629ed2a9072f12b00cb90eb5845f4c0
AliHassanL12 <liaqqahl@gmail.com>
2024-10-16 22:41:07 +0100

    Create calculator layout

---
+++ javascript/javascript.js
@@ -0,0 +1,43 @@
+/*
+Your calculator is going to contain functions for all of the basic math operators you
+typically find on calculators, so start by creating functions for the following items 
+and testing them in your browser’s console.
+
+add
+subtract
+multiply
+divide
+
+*/
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let num1, num2, operator;
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
